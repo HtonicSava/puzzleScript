f0b459b0210a296940400dd19cca31abec5828c6
HtonicSava <htonic.sava@gmail.com>
2021-08-20 17:16:29 +0300

    upd

---
+++ puzzle_script/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <Menu />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Menu from '@/components/menu.vue';
+
+export default {
+  name: 'Login',
+  components: {
+    Menu
+  }
+}
+</script>
