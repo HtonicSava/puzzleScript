7dc58bbadd97b5710907c2533888e17f4bff2619
HtonicSava <htonic.sava@gmail.com>
2021-08-20 18:18:50 +0300

    header

---
+++ src/components/header.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="header">
+       <div class="header__content">
+         <div class="header__logo">
+           <img src="../assets/img/header/session.png" alt="">
+         </div>
+         <div class="header__nav">
+           <p>Бета-тест</p>
+           <p>Принцип работы</p>
+           <p>Преимущества</p>
+           <p>Команда</p>
+         </div>
+       </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'header',
+    data: function () {
+
+    }
+     
+  }
+</script>
+
+<style lang="scss" scoped>
+    .header{
+        color: red;
+    }
+</style>
