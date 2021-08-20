b3d690061f5328595abe732fbe45a047466d2f52
HtonicSava <htonic.sava@gmail.com>
2021-08-20 18:15:41 +0300

    main

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
-    <Menu />
+
   </div>
 </template>
 
 <script>
 import axios from '@/service/axios';
-import Menu from '@/components/menu.vue';
+
 
 export default {
   name: 'Login',
   components: {
-    Menu
+
   },
   methods: {
     login() {
---
+++ src/views/Main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="main">
+    <Header />
+  </div>
+</template>
+
+<script>
+import Header from '@/components/header.vue';
+
+export default {
+  name: 'Main',
+  components: {
+    Header
+  },
+  methods: {
+    
+  }
+}
+</script>
