fc16d5efd21b48b9dba187050554d5395c05ea9a
Yarosalw <yarosalw274@gmail.com>
2021-08-20 18:56:58 +0300

    Update

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,19 +1,39 @@
 <template>
   <div class="main">
     <Header />
+
+    <div class="mt-4 wrapper">
+
+    </div>
+
+    <modalLogin
+      v-model="flag_modal_login"
+    />
+
+    <div @click="toggleModalLogin">OpenLogin</div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/header.vue';
+import modalLogin from '@/components/Login.vue';
 
 export default {
   name: 'Main',
+  data() {
+    return {
+      flag_modal_login: false
+    }
+  },
   components: {
-    Header
+    Header,
+
+    modalLogin,
   },
   methods: {
-    
+    toggleModalLogin() {
+      this.flag_modal_login = !this.flag_modal_login;
+    }
   }
 }
 </script>
