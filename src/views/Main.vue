a21bbfcdfc6b139561ac231bbc21197703646cae
HtonicSava <htonic.sava@gmail.com>
2021-08-20 20:38:49 +0300

    description is added to main

--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="main">
     <Header />
+    <Description />
     <div class="mt-4 wrapper">
 
     </div>
@@ -9,41 +10,31 @@
       v-model="flag_modal_login"
     />
 
-    <modalRegistery
-      v-model="flag_modal_registery"
-    />
-
     <div @click="toggleModalLogin">OpenLogin</div>
-    <div @click="toggleModalRegistery">OpenRegistery</div>
   </div>
 </template>
 
 <script>
 import Header from '@/components/header.vue';
 import modalLogin from '@/components/Login.vue';
-import modalRegistery from '@/components/Registery.vue';
+import Description from '@/components/Description.vue';
 
 export default {
   name: 'Main',
   data() {
     return {
-      flag_modal_login: false,
-      flag_modal_registery: false,
+      flag_modal_login: false
     }
   },
   components: {
     Header,
-
+    Description,
     modalLogin,
-    modalRegistery
   },
   methods: {
     toggleModalLogin() {
       this.flag_modal_login = !this.flag_modal_login;
-    },
-    toggleModalRegistery() {
-      this.flag_modal_registery = !this.flag_modal_registery;
-    },
+    }
   }
 }
 </script>
